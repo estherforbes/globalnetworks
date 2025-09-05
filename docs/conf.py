bb20fdff8042979f4787112d5974b233e6ab2beb
jenniemcgowan <jenniemcgowan@hotmail.com>
2025-09-05 12:09:12 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Sbcglobal Central'
+project = 'Global Networks'
 copyright = '2025'
-author = 'Cheryl Lanham'
+author = 'Margaret Frazer'
 
 release = '1.0'
 
@@ -21,3 +21,4 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
